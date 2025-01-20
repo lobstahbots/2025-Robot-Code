3f235d08ad38b67b089cfd74fb005af61e6cba99
Lauraleam1a <lsadka@bu.edu>
2025-01-20 17:33:00 -0500

    Created intake roller and intake pivot methods

--- src/main/java/frc/robot/subsystems/algaeIntake/AlgaeIntake.java
+++ src/main/java/frc/robot/subsystems/algaeIntake/AlgaeIntake.java
@@ -5,17 +5,44 @@
 package frc.robot.subsystems.algaeIntake;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class AlgaeIntake extends SubsystemBase {
   private final CANSparkMax intakeRoller;
-
+  private final CANSparkMax intakePivot;
   /** Creates a new AlgaeIntake. */
-  
-  public AlgaeIntake() {}
-  
+  public AlgaeIntake(int intakeRollerID, int intakePivotID) {
+    intakeRoller = new CANSparkMax(intakeRollerID, MotorType.kBrushless);
+    intakePivot = new CANSparkMax(intakePivotID, MotorType.kBrushless);
+    intakeRoller.setIdleMode(IdleMode.kBrake);
+    intakePivot.setIdleMode(IdleMode.kBrake);
+    intakeRoller.setSmartCurrentLimit(40);
+    intakePivot.setSmartCurrentLimit(40);
+    intakeRoller.setInverted(false);
+    intakePivot.setInverted(false);
+  }
+
+  public void setIntakeRollerSpeed(double speed){
+    intakeRoller.set(speed);
+  }
+
+  public void setIntakePivotSpeed(double speed){
+    intakePivot.set(speed);
+  }
+
+  public void stopIntakeRoller(){
+    intakeRoller.stopMotor();
+  }
+
+  public void stopIntakePivot(){
+    intakePivot.stopMotor();
+  }
+
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
 }
