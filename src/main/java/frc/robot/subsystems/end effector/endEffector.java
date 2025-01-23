449ce142a9c9242e2fc0f25ca08d95dbbe9da2d2
aacna2109 <alyalv@bu.edu>
2025-01-23 16:34:19 -0500

    Made requested changes for #2

--- src/main/java/frc/robot/subsystems/end effector/endEffector.java
+++ src/main/java/frc/robot/subsystems/end effector/endEffector.java
@@ -1,17 +1,30 @@
-public class endEffector extends SubsystemBase{
-    private final CANSparkMax endEffectorMotor;
-    private final int MOTOR_SPEED;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.EndEffectorConstants;
 
-    public endEffector(int endEffectorMotorID) {
-        endEffectorMotor = new CANSparkMaxSparkMax(endEffectorMotorID);
-        MOTOR_SPEED = 2
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+public class EndEffector extends SubsystemBase {
+  private final SparkMax endEffectorMotor;
+
+  public EndEffector(int endEffectorMotorID) {
+    endEffectorMotor = new SparkMax(endEffectorMotorID, MotorType.kBrushless);
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.smartCurrentLimit(40);
+    config.idleMode(IdleMode.kBrake);
+    config.inverted(false);
+    endEffectorMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
   public void stopMotor() {
     endEffectorMotor.stopMotor();
-    }
+  }
 
-  public void runMotor () {
-    endEffectorMotor.set(MOTOR_SPEED);
+  public void runMotor() {
+    endEffectorMotor.set(EndEffectorConstants.MOTOR_SPEED);
   }
 }
