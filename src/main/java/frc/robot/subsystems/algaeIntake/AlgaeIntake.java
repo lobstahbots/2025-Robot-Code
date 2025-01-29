e00f6e2c658843a5c1b4f551ada1c51bf1651868
Lauraleam1a <lsadka@bu.edu>
2025-01-29 11:24:51 -0500

    Fixed PR feedback

--- src/main/java/frc/robot/subsystems/algaeIntake/AlgaeIntake.java
+++ src/main/java/frc/robot/subsystems/algaeIntake/AlgaeIntake.java
@@ -5,40 +5,46 @@
 package frc.robot.subsystems.algaeIntake;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
+import java.security.PermissionCollection;
+
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 public class AlgaeIntake extends SubsystemBase {
-  private final CANSparkMax intakeRoller;
-  private final CANSparkMax intakePivot;
+  private final SparkMax intakeRoller;
+  private final SparkMax intakePivot;
   /** Creates a new AlgaeIntake. */
   public AlgaeIntake(int intakeRollerID, int intakePivotID) {
-    intakeRoller = new CANSparkMax(intakeRollerID, MotorType.kBrushless);
-    intakePivot = new CANSparkMax(intakePivotID, MotorType.kBrushless);
-    intakeRoller.setIdleMode(IdleMode.kBrake);
-    intakePivot.setIdleMode(IdleMode.kBrake);
-    intakeRoller.setSmartCurrentLimit(40);
-    intakePivot.setSmartCurrentLimit(40);
-    intakeRoller.setInverted(false);
-    intakePivot.setInverted(false);
+    intakeRoller = new SparkMax(intakeRollerID, MotorType.kBrushless);
+    intakePivot = new SparkMax(intakePivotID, MotorType.kBrushless);
+    SparkMaxConfig config = new SparkMaxConfig();
+    config.smartCurrentLimit(20);
+    config.idleMode(IdleMode.kBrake);
+    config.inverted(false);
+    intakeRoller.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    intakePivot.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
 
-  public void setIntakeRollerSpeed(double speed){
+  public void setIntakeRollerSpeed(double speed) {
     intakeRoller.set(speed);
   }
 
-  public void setIntakePivotSpeed(double speed){
+  public void setIntakePivotSpeed(double speed) {
     intakePivot.set(speed);
   }
 
-  public void stopIntakeRoller(){
+  public void stopIntakeRoller() {
     intakeRoller.stopMotor();
   }
 
-  public void stopIntakePivot(){
+  public void stopIntakePivot() {
     intakePivot.stopMotor();
   }
-
 
   @Override
   public void periodic() {
