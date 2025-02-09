6bcc09729e052f767aa36386920729ed585b4fe7
pothos-not-pothos <hkaneko137@gmail.com>
2025-02-09 14:41:14 -0500

    oops extra curly bracket

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -253,7 +253,7 @@ public final class Constants {
   }
 
   public static class PivotConstants {
-    public static final double kP = .1;
+    public static final double kP = 0.1;
     public static final double kI = 0;
     public static final double kD = 0;
     public static final double kS = 0;
@@ -264,5 +264,5 @@ public final class Constants {
 
 
 }
-}
+
   
