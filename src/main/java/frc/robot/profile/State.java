f30fa72d3c659dc65ed91751587259355736f758
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2025-03-18 14:40:40 -0400

    add regular state record

---
+++ src/main/java/frc/robot/profile/State.java
@@ -0,0 +1,22 @@
+package frc.robot.profile;
+
+/**
+ * A record to store the state (position/velocity) of two degrees of freedom
+ */
+public record State(
+        /**
+         * The position of the first DOF
+         */
+        double dof1Pos,
+        /**
+         * The velocity of the first DOF
+         */
+        double dof1Vel,
+        /**
+         * The position of the second DOF
+         */
+        double dof2Pos,
+        /**
+         * The velocity of the second DOF
+         */
+        double dof2Vel) {}
