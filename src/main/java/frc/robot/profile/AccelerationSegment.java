0356451bd4af926dadb139370032e225644b6b77
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2025-03-18 18:58:12 -0400

    add helper method for deceleration

--- src/main/java/frc/robot/profile/AccelerationSegment.java
+++ src/main/java/frc/robot/profile/AccelerationSegment.java
@@ -28,7 +28,7 @@ public class AccelerationSegment implements DualDOFProfileSegment {
      * @param dof2Vel      the final velocity of the second DOF
      * @param dof1AccMax   the maximum acceleration of the first DOF; must be
      *                     positive
-     * @param dof2AccMax   the maximum accleration of the second DOF; must be
+     * @param dof2AccMax   the maximum acceleration of the second DOF; must be
      *                     positive
      */
     public AccelerationSegment(DualDOFState initialState, double dof1Vel, double dof2Vel, double dof1AccMax,
@@ -38,6 +38,32 @@ public class AccelerationSegment implements DualDOFProfileSegment {
                 Math.abs(dof2Vel - initialState.dof2Vel()) / dof2AccMax);
         this.dof1Acc = (dof1Vel - initialState.dof1Vel()) / duration;
         this.dof2Acc = (dof2Vel - initialState.dof2Vel()) / duration;
+    }
+
+    /**
+     * Create an acceleration segment which will decelerate to a specified position
+     * from specified velocities with specified (positive) acceleration limits. That
+     * is, specified velocities can be negative, but specified acceleration limits
+     * are treated as limits on the absolute value of acceleration.
+     * 
+     * @param decelerateTo the position you want to end up at
+     * @param dof1Vel      the velocity of the first DOF to decelerate from
+     * @param dof2Vel      the velocity of the second DOF to decelerate from
+     * @param dof1AccMax   the maximum acceleration of the first DOF; must be
+     *                     positive
+     * @param dof2AccMax   the maximum acceleration of the second DOF; must be
+     *                     negative
+     * @return the created acceleration segment
+     */
+    public static AccelerationSegment getDeclerationSegment(DualDOFPositionState decelerateTo, double dof1Vel,
+            double dof2Vel, double dof1AccMax, double dof2AccMax) {
+        double duration = Math.max(Math.abs(dof1Vel) / dof1AccMax, Math.abs(dof2Vel) / dof2AccMax);
+        double dof1Acc = -dof1Vel / duration;
+        double dof2Acc = -dof2Vel / duration;
+        return new AccelerationSegment(
+                new DualDOFState(decelerateTo.dof1Pos() - duration * duration * dof1Acc / 2,
+                        decelerateTo.dof2Pos() - duration * duration * dof2Acc / 2, dof1Vel, dof2Vel),
+                0, 0, dof1Acc, dof2Acc);
     }
 
     public double getDuration() {
@@ -55,7 +81,6 @@ public class AccelerationSegment implements DualDOFProfileSegment {
     public DualDOFState calculate(double time) {
         return new DualDOFState(initialState.dof1Pos() + time * initialState.dof1Vel() + time * time * dof1Acc / 2,
                 initialState.dof2Pos() + time * initialState.dof2Vel() + time * time * dof2Acc / 2,
-                initialState.dof1Vel() + time * dof1Acc,
-                initialState.dof2Vel() + time * dof2Acc);
+                initialState.dof1Vel() + time * dof1Acc, initialState.dof2Vel() + time * dof2Acc);
     }
 }
