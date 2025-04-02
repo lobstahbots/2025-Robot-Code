75b5b7450d27d11832cae1c1d32fa10e425e2d86
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2025-04-02 15:22:05 -0400

    use correct logic

--- src/main/java/frc/robot/commands/drivebase/AlignToBargeCommand.java
+++ src/main/java/frc/robot/commands/drivebase/AlignToBargeCommand.java
@@ -7,7 +7,6 @@ package frc.robot.commands.drivebase;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -16,20 +15,22 @@ import frc.robot.Constants.FieldConstants;
 import frc.robot.subsystems.drive.DriveBase;
 import frc.robot.util.trajectory.AlliancePoseMirror;
 
-/* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
+/*
+ * You should consider using the more terse Command factories API instead
+ * https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-
+ * command-based.html#defining-commands
+ */
 public class AlignToBargeCommand extends Command {
     /** Creates a new AlignToBargeCommand. */
     private final PIDController xController = new PIDController(DriveConstants.AUTO_ALIGN_TRANSLATION_kP,
             DriveConstants.AUTO_ALIGN_TRANSLATION_kI, DriveConstants.AUTO_ALIGN_TRANSLATION_kD);
-    
+
     private final PIDController thetaController = new PIDController(DriveConstants.ROTATION_PID_CONSTANTS.kP,
-        DriveConstants.ROTATION_PID_CONSTANTS.kI, DriveConstants.ROTATION_PID_CONSTANTS.kD);
+            DriveConstants.ROTATION_PID_CONSTANTS.kI, DriveConstants.ROTATION_PID_CONSTANTS.kD);
 
     private final DriveBase driveBase;
 
     private final DoubleSupplier ySupplier;
-    
-    private Pose2d targetPose;
 
     public AlignToBargeCommand(DriveBase driveBase, DoubleSupplier ySupplier) {
         // Use addRequirements() here to declare subsystem dependencies.
@@ -45,19 +46,21 @@ public class AlignToBargeCommand extends Command {
         xController.reset();
         thetaController.reset();
 
-        targetPose = AlliancePoseMirror.mirrorPose2d(new Pose2d(FieldConstants.Poses.BARGE_TRANSLATION_DEPTH_SETPOINT, targetPose.getY(), Rotation2d.fromRadians(0)));
-
-        xController.setSetpoint(targetPose.getX());
-        thetaController.setSetpoint(targetPose.getRotation().getRadians());
+        xController.setSetpoint(AlliancePoseMirror.isRedAlliance()
+                ? FieldConstants.FIELD_LENGTH - FieldConstants.Poses.BARGE_TRANSLATION_DEPTH_SETPOINT
+                : FieldConstants.Poses.BARGE_TRANSLATION_DEPTH_SETPOINT);
+        thetaController.setSetpoint(
+                AlliancePoseMirror.isRedAlliance() ? Rotation2d.k180deg.getRadians() : Rotation2d.kZero.getRadians());
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        driveBase.driveRobotRelative(ChassisSpeeds.fromFieldRelativeSpeeds(
-                xController.calculate(driveBase.getPose().getX()), ySupplier.getAsDouble(),
-                thetaController.calculate(driveBase.getPose().getRotation().getRadians()),
-                driveBase.getPose().getRotation()));
+        driveBase.driveRobotRelative(
+                ChassisSpeeds.fromFieldRelativeSpeeds(xController.calculate(driveBase.getPose().getX()),
+                        AlliancePoseMirror.isRedAlliance() ? -ySupplier.getAsDouble() : ySupplier.getAsDouble(),
+                        thetaController.calculate(driveBase.getPose().getRotation().getRadians()),
+                        driveBase.getPose().getRotation()));
     }
 
     // Called once the command ends or is interrupted.
